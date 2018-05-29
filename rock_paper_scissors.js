3135c4706543294477287f731f317f2695174e88
kris hays <krisjhays@gmail.com>
2018-05-28 21:14:24 -0400

    worked the bugs out of the loop

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,25 +1,28 @@
-let player ="rock";
-let com = ["rock","paper","scissors"];
+let player = "rock";
+let com = ["rock", "paper", "scissors"];
 let computer = com[Math.floor(Math.random() * com.length)];
 let totalPlayer = 0;
 let totalComputer = 0;
 
 while (true) {
     if (player === "rock") {
-        if (computer === "paper") {
+        if (computer === "paper")
             totalComputer += 1;
-        } else 
-            totalPlayer += 1;
-        }  if (player === "paper") {
-            if (computer === "scissors")
-                totalComputer += 1;
-        } else {
-            totalPlayer += 1;
-        }  if (player === "scissors") {
-            if (computer === "rock")
-                totalComputer += 1;
-        } else {
-            totalPlayer += 1;
-        } 
-    }   break;
+     else 
+        totalPlayer += 1;
+    }
+    if (player === "paper") {
+        if (computer === "scissors")
+            totalComputer += 1;
+     else 
+        totalPlayer += 1;
+    }
+    if (player === "scissors") {
+        if (computer === "rock")
+            totalComputer += 1;
+     else 
+        totalPlayer += 1;
+    }
+
+    break;
 };
