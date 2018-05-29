0d0cc155d71d42bdc2aeaabf60109acf7c913d1d
kris hays <krisjhays@gmail.com>
2018-05-28 20:40:03 -0400

    javascript rock game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,7 +1,6 @@
-import random
-let player = prompt(["rock, paper or scissors"]);
-let com = "rock","paper","scissors";
-let computer = com.charAt(math.random(com));
+let player ="rock";
+let com = ["rock","paper","scissors"];
+let computer = com[Math.floor(Math.random() * com.length)];
 let totalPlayer = 0;
 let totalComputer = 0;
 
@@ -9,17 +8,18 @@ while (true) {
     if (player === "rock") {
         if (computer === "paper") {
             totalComputer += 1;
-        } else {
+        } else 
             totalPlayer += 1;
-        } else if (player === "paper") {
+        }  if (player === "paper") {
             if (computer === "scissors")
                 totalComputer += 1;
         } else {
             totalPlayer += 1;
-        } else if (player === "scissors") {
+        }  if (player === "scissors") {
             if (computer === "rock")
                 totalComputer += 1;
         } else {
             totalPlayer += 1;
-        } else { }
-    }   break
+        } 
+    }   break;
+};
