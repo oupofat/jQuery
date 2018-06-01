a7681e74c2b2c2dd89a03d21234b979742170e61
kris hays <krisjhays@gmail.com>
2018-05-31 21:00:54 -0400

    pushing work

--- rock_paper.js
+++ rock_paper.js
@@ -1,58 +1,74 @@
-//Working on trying to get the computer to right. not sure what to do. 
-
+//when the player button rock is pushed it returns the playComputerMove function. then prints it.
 $("#play-rock").on("click", function () {
-    $("#computer-move").text(playComputerMove(com));
-
-    function playComputerMove(com) {
-        let com = ["rock", "paper", "scissors"];
-        let computer = com[Math.floor(Math.random() * com.length)];
-    }
+    $("#computer-move").text(playComputerMove());
+});
+//when the player button paper is pushed it returns the playComputerMove function. then prints it.
+$("#play-paper").on("click", function () {
+    $("#computer-move").text(playComputerMove());
+});
+//when the player button scissors is pushed it returns the playComputerMove function. then prints it.
+$("#play-scissors").on("click", function () {
+    $("#computer-move").text(playComputerMove());
 });
 
-//this is going to show when the button rock is pushed it shows up in the player pick.
-
-$("#play-rock").on("click", function () {
-    $("#player-move").text("rock");
-});
 //this plays the rock button.
 
 $("#play-rock").on("click", function () {
     $("#player-move").text("rock");
-
-    function runGame(rock) {
-        if (player === "rock" && computer === "paper") {
-            $("#game-result").text("you lose");
-            if (computer === "rock") {
-                $("#game-result").text("It's a tie!");
-            } else {
-                $("#game-result").text("You Win!");
-            }
-        }
-    }
+    let player = ("rock");
 });
+
 //this plays the paper button
 
 $("#play-paper").on("#click", function () {
-    function runGame(paper) {
-        if (player === "paper" && computer === "scissors") {
-            $("#game-result").text("You lose!");
-            else if (computer === "paper") {
-                $("#game-result").text("It's a tie!");
-            } else {
-                $("#game-result").text("You win!");
-            }
+    $("#player-move").text("paper");
+    let player = ("paper");
+});
+
+//this plays the scissors button
+$("#play-scissors").on("#click", function () {
+    $("#player-move").text("scissors");
+    let player = ("scissors")
+});
+// computer function move this 
+function playComputerMove() {
+    let com = ["rock", "paper", "scissors"]
+    let computer = com[Math.floor(Math.random() * com.length)]
+    console.log(computer);
+    return (computer);
+};
+
+//this function preforms the rock part of the game
+function runGame(rock) {
+    if (player === "rock" && computer === "paper") {
+        $("#game-result").text("you lose");
+        if (computer === "rock") {
+            $("#game-result").text("It's a tie!");
+        } else {
+            $("#game-result").text("You Win!");
         }
     }
-});
-//this plays the scissors button
+};
+
+//this function preforms the paper part of the game
+function runGame(paper) {
+    if (player === "paper" && computer === "scissors") {
+        $("#game-result").text("You lose!");
+    } else if (computer === "paper") {
+        $("#game-result").text("It's a tie!");
+    } else {
+        $("#game-result").text("You win!");
+    }
+};
+
+//this function preforms the scissors part of the game
 
 function runGame(scissors) {
     if (player === "scissors" && computer === "rock") {
         $("#game-result").text("You lose!");
-        else if (computer === "scissors") {
-            $("#game-result").text("It's a tie!");
-        } else {
-            $("#game-result").text("You win!");
-        }
+    } else if (computer === "scissors") {
+        $("#game-result").text("It's a tie!");
+    } else {
+        $("#game-result").text("You win!");
     }
-}
+};
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -3,36 +3,37 @@ let com = ["rock", "paper", "scissors"];
 let computer = com[Math.floor(Math.random() * com.length)];
 let totalPlayer = 0;
 let totalComputer = 0;
-
+if (totalPlayer > totalComputer) {
+    console.log("you win!");
+} else if (totalPlayer < totalComputer) {
+    console.log("you lose!");
+} else if (totalPlayer = totalComputer) {
+    console.log("its a tie.")
+}
 while (true) {
     if (player === "rock") {
         if (computer === "paper")
             totalComputer += 1;
-        else if (computer === "rock") {} 
-        else
+        else if (computer === "rock") {} else
             totalPlayer += 1;
     }
     if (player === "paper") {
         if (computer === "scissors")
             totalComputer += 1;
-        else if (computer === "scissors") {} 
-        else
+        else if (computer === "scissors") {} else
             totalPlayer += 1;
     }
     if (player === "scissors") {
         if (computer === "rock")
             totalComputer += 1;
-        else if (computer === "scissors") {} 
-        else
+        else if (computer === "scissors") {} else
             totalPlayer += 1;
     }
 
     break;
 };
-if (totalPlayer > totalComputer){
+if (totalPlayer > totalComputer) {
     console.log("you win!")
-} else if (totalPlayer < totalComputer) {
-    console.log("you lose!")
 } else {
-    console.log ("Its a tie!")
+    console.log("you lose!")
 }
